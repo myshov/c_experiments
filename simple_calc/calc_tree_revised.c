8ea681b58ee744c939f92c67beedfee83e4b4dc0
Alexander Myshov <alexander.myshov@gmail.com>
2014-04-28 14:46:18 +0900

    test of regular tree

--- simple_calc/calc_tree_revised.c
+++ simple_calc/calc_tree_revised.c
@@ -6,11 +6,11 @@
 #define BUFSIZE 100
 #define MAXWORD 100
 
-enum {TASK_END, EXIT};
+enum {NUMBER, TASK_END, EXIT};
 
 typedef struct tnode {
     char operator;
-    int operand;
+    double operand;
     struct tnode *top;
     struct tnode *left;
     struct tnode *right;
@@ -19,8 +19,14 @@ typedef struct tnode {
 int getword(char *, int);
 char getch(void);
 void ungetch(char);
-//struct tnode *addtree(struct tnode *, char *);
-//struct tnode *talloc(void);
+node *add_node(node *p, char *w);
+node *talloc(void);
+node *parse_parensis(void);
+node *parse_expression(void);
+double calculate(node *part);
+
+node *g_current_node = NULL;
+node *g_current_op_node = NULL;
 
 /* externals for getch and ungetch functions */
 char buf[BUFSIZE];
@@ -82,33 +88,136 @@ int getword(char *word, int lim) {
             }
             w++;
         }
+    } else {
+        *w++ = c;
     }
     *w = '\0';
     return word[0];
 }
 
-node *parse_expression() {
-    /* code */
+int is_operation(int o) {
+    if ((o == '+') || (o == '-') ||
+        (o == '*') || (o == '/')) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int is_expression(int p) {
+    if ((isnumber(p)) || is_operation(p)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/* add_node */
+node *add_node(node *p, char *w) {
+    int cond;
+    if (p == NULL) {
+        if (is_operation(w[0])) {
+            p = talloc();
+            p->operator = w[0];
+            p->left = p->right = p->top = NULL;
+        } else if (isnumber(w[0])) {
+            p = talloc();
+            p->operator = 0;
+            p->operand = atof(w);
+            p->left = p->right = p->top = NULL;
+        }
+    }
+    return p;
+}
+
+/* talloc: make a tnode */
+node *talloc(void) {
+    return (node *) malloc(sizeof(node));
+}
+
+double calculate(node *part) {
+    double op1, op2;
+    double result;
+    if (part->left) {
+        op1 = calculate(part->left);
+    } else {
+        return part->operand;
+    }
+    if (part->right) {
+        op2 = calculate(part->right);
+    } else {
+        return part->operand;
+    }
+    switch (part->operator) {
+        case '+':
+            result = op1 + op2;
+            break;
+        case '-':
+            result = op1 - op2;
+            break;
+        case '*':
+            result = op1 * op2;
+            break;
+        case '/':
+            result = op1 / op2;
+            break;
+    }
+    return result;
+}
+
+node *parse_expression(void) {
+    int word_type;
+    char current_word[MAXWORD];
+    node l_node;
+
+    while ((word_type = getword(current_word, MAXWORD)) != EXIT) {
+        if (word_type == '(') {
+            parse_parensis();
+        } else if (is_expression(word_type)) {
+            // write number because of first part of expression
+            // which always a number
+            if (!g_current_node) {
+                g_current_node = add_node(NULL, current_word);
+            } else if (is_operation(word_type)) {
+                g_current_op_node = add_node(NULL, current_word);
+                g_current_op_node->left = g_current_node;
+                g_current_node->top = g_current_op_node;
+                g_current_node = g_current_op_node;
+            } else if (isnumber(current_word[0])) {
+                g_current_node->right = add_node(NULL, current_word);
+                g_current_node->right->top = g_current_node;
+            }
+        
+
+        } else if (word_type == TASK_END) {
+            printf("-----------\n");
+            printf("calculation\n");
+            printf("result: %f\n", calculate(g_current_node));
+        } else {
+            printf("error: %s is wrong expression\n", current_word);
+        }
+    }
     return NULL;
 }
 
-node *parse_parensis() {
-    /* code */
+node *parse_parensis(void) {
+    int word_type;
+    char current_word[MAXWORD];
+
+    word_type = getword(current_word, MAXWORD);
+    if (word_type == '(') {
+        printf("parse_parensis()\n");
+        parse_parensis();
+    } else {
+        // write number
+        printf("parse_expression()\n");
+        parse_expression();
+    }
+
     return NULL;
 }
 
 int main(int argc, const char *argv[]) {
-    printf("%d\n", TASK_END);
-    printf("%d\n", EXIT);
-    int word_type;
-    char current_word[MAXWORD];
-
-    while ((word_type = getword(current_word, MAXWORD)) != EXIT) {
-        if (word_type == TASK_END) {
-            printf("\n");
-        } else {
-            printf("%s ", current_word);
-        }
-    }
+    parse_expression();
     return 0;
 }
